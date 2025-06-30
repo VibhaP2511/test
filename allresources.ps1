aa6c3371852dde74080a68b4fcb562ac037f0d3d
VibhaP2511 <vibhap510@gmail.com>
2025-06-30 18:24:25 +0530

    Update allresources.ps1

--- allresources.ps1
+++ allresources.ps1
@@ -1,52 +1,95 @@
-# Ensure you are logged in to Azure
+# Install-Module -Name Az -AllowClobber -Force
+# Import-Module -Name Az  -Force
  
-# Get all subscriptions in the Azure account
+$script:resourceData = @()
 $subscriptions = Get-AzSubscription
- 
-# Initialize array to store resource data
-$resourceData = @()
- 
-# Iterate through each subscription
-foreach ($subscription in $subscriptions) {
-Set-AzContext -SubscriptionId $subscription.Id
- 
-    # Get all resources in the current subscription
-    $resources = Get-AzResource
- 
-    foreach ($resource in $resources) {
-        if ($resource.Tags) {
-            foreach ($tag in $resource.Tags.GetEnumerator()) {
-                $resourceData += [PSCustomObject]@{
-                    SubscriptionID = $subscription.Id
-                    SubscriptionName = $subscription.Name
-                    ResourceID       = $resource.ResourceId
-                    ResourceName = $resource.Name
-                    ResourceType     = $resource.ResourceType
-                    ResourceLocation = $resource.Location
-                    ResourceGroup    = $resource.ResourceGroupName
-                    TagName          = $tag.Key
-                    TagValue         = $tag.Value
-                }
-            }
-        } else {
-            # If no tags, add resource with empty Tag Name & Value
-            $resourceData += [PSCustomObject]@{
-            SubscriptionID = $subscription.Id
-            SubscriptionName = $subscription.Name
-            ResourceID       = $resource.ResourceId
-            ResourceName = $resource.Name
-            ResourceType     = $resource.ResourceType
-            ResourceLocation = $resource.Location
-            ResourceGroup    = $resource.ResourceGroupName
-            TagName          = ""
-            TagValue         = ""
-            }
-        }
+foreach($subscription in $subscriptions)
+{
+    Set-AzContext -Subscription $subscription.Id
+    try{   
+    $allResources = Get-AzResource
+    $resourceD = @()
+    foreach($resource in $allResources){       
+        $dataRow = [pscustomobject]@{                        
+                        "Subscription Name" = $subscription.Name
+                        "Resource Name" = $resource.Name
+                        "Resource Group Name" = $resource.ResourceGroupName
+                        "Resource Type" = $resource.ResourceType
+                        "Location" = $resource.location
+                        "Resource Id" = $resource.ResourceId                  
+                    }
+        $resourceD += $dataRow
     }
+    $script:resourceData += $resourceD 
+    }catch{
+ 
+    Write-Output "Error while fetching all resources - $($error[0])"
+ 
+   }
 }
+$script:resourceData | Export-Csv -Path "Azure_All_Resources.csv" -Force -NoTypeInformation -Encoding UTF8
  
-# Export to CSV
-$csvPath = "./reports/AzureResourceTagReport.csv"
-$resourceData | Export-Csv -Path $csvPath -NoTypeInformation
  
-Write-Output "Resource tag report saved to: $csvPath"
+# $fileName = "Azure_All_Resources.csv"
+# $authority = "https://login.microsoftonline.com/189de737-c93a-4f5a-8b68-6f4ca9941912/oauth2/token"
+# $resourceUrl = "https://communication.azure.com"
+
+# $body = @{grant_type = "client_credentials"
+#           client_id = $clientId
+#           client_secret = $pass
+#           resource = 'https://communication.azure.com'
+# }
+# $response = Invoke-WebRequest -Method Post -Uri $authority -Body $body -ContentType 'application/x-www-form-urlencoded' -UseBasicParsing
+# $accessToken = $response.Content | ConvertFrom-Json
+# $accessToken.access_token
+# $uuid = [guid]::NewGuid().ToString()
+# $gmt = get-date -format U
+# $base64_csv = [Convert]::ToBase64String([IO.File]::ReadAllBytes($($fileName)))
+ 
+# $params = @{
+#     Method = 'POST'
+#     URI = 'https://eops-acs.australia.communication.azure.com/emails:send?api-version=2023-03-31'
+#     Headers = @{
+#         Authorization = 'Bearer ' + $accessToken.access_token
+#         'Content-Type' = 'application/json'
+#         'repeatability-first-sent' = $gmt
+#         'repeatability-request-id' = $uuid 
+#     }
+#     Body = @{
+#         senderAddress = 'hcl-elasticops@aa34110f-dc60-463e-b5cf-bd3b7c8ce04d.azurecomm.net'
+#         Content = @{
+#             Subject = 'Azure | All Resources'
+#             PlainText = 'used azure communication service'
+#             html = "<html><head><title> !</title></head><body><p>Greetings,</p>
+# <p>Please find attached Azure All Resources Report for your reference.</p>
+# <p>
+#                         Note that this is automatically generated email via Github Actions. For any queries or concerns, please reach out at AUTONOMICS-DEVOPS@HCL.COM</p>
+# <p><br>Regards,</p>
+# <p>EOPS AUTONOMICS</p>
+# </body></html>"
+#         }
+#         recipients = @{
+#             To = @(
+#                 @{
+#                 address = 'padam.sinha@hcltech.com'
+#                 displayName = 'Padam'
+#                 }
+
+#             )
+#             Cc = @(
+#                @{
+#                 address = 'padam.sinha@hcl.com'
+#                 displayName = 'Sinha'
+#                 }
+#             )
+#         }
+#         Attachments = @(
+#            @{ 
+#             name = $($fileName)
+#             contentType = "text/csv"
+#             contentInBase64 = $($base64_csv)
+#            }
+#         )
+#     } | ConvertTo-Json -Depth 100
+# }
+# (Invoke-WebRequest @params -UseBasicParsing).RawContent
